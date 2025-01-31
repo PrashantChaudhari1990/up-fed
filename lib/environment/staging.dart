4d19165c610f3881b005cdeefc15e3346cc9e425
Vishnu Bhangale <vishnu.bhangale@oorjaa.tech>
2025-01-31 15:51:12 +0530

    Change dev api url and add stage environment

--- lib/environment/staging.dart
+++ lib/environment/staging.dart
@@ -2,7 +2,7 @@ import 'environment.dart';
 
 class Staging extends Environment{
   Staging() : super(
-      baseUrl: 'https://stage.oorjaa.tech',
-      webAppUrl: 'http://dev.oorjaa.tech/oorjaa'
+      baseUrl: 'http://74.225.188.40:8006',
+      webAppUrl: 'http://www.kh-shop.stage.oorjaa.tech'
   );
 }
