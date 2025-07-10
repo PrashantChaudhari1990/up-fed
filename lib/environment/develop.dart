aa49957bcb64ec7f17f296c870fccbef2aeef33e
prashant chaudhari <prashant.chaudhari@oorjaa.tech>
2025-07-10 13:51:49 +0530

    env url changes

--- lib/environment/develop.dart
+++ lib/environment/develop.dart
@@ -2,7 +2,7 @@ import 'environment.dart';
 
 class Development extends Environment{
   Development() : super(
-      baseUrl: 'https://dev.oorjaa.tech:8089',
+      baseUrl: 'https://tms-api.dev.oorjaa.tech',
       webAppUrl: 'https://vendor-partner.dev.oorjaa.tech' //not confirm
   );
 }
