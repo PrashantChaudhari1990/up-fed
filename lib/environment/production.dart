22a12d5925387533085c6ce3f3c19af72f9916d4
prashant chaudhari <prashant.chaudhari@oorjaa.tech>
2025-09-12 14:29:58 +0530

    icons changes

--- lib/environment/production.dart
+++ lib/environment/production.dart
@@ -3,7 +3,7 @@ import 'environment.dart';
 class Production extends Environment {
   Production()
       : super(
-            baseUrl: 'vas-connect-api.oorjaa.tech',
-            webAppUrl: 'https://bttoa-connect.oorjaa.tech',
-            appUpdate: true);
+      baseUrl: 'https://vas-connect-api.oorjaa.tech',
+      webAppUrl: 'https://bttoa-connect.oorjaa.tech',
+      appUpdate: true);
 }
